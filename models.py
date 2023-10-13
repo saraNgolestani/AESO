612d1b1e3c4662f4e847ec12ac8e148e37e5246f
Sara Naseri Golestani <sarah.gln.97@gmail.com>
2023-10-13 16:45:45 -0600

    model training

--- models.py
+++ models.py
@@ -1,9 +1,35 @@
+from array import array
+import numpy as np
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
 
-def init():
-    return
 
-def train():
-    return
+def train(param_grid: {}, model: GradientBoostingRegressor, x_train: array, y_train: array) -> GradientBoostingRegressor:
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
 
-def predict():
+    # Fit the grid search to the training data
+    grid_search.fit(x_train, y_train)
+
+    # Get the best hyperparameters
+    best_params = grid_search.best_params_
+    print("Best Hyperparameters:", best_params)
+
+    # Get the best model
+
+    return grid_search.best_estimator_
+
+
+def predict(model: GradientBoostingRegressor, x_test: array, y_test: array):
+    # Make predictions with the best model
+    y_pred = model.predict(x_test)
+
+    # Evaluate the best model
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print("Mean Squared Error:", mse)
+    print("Mean Absolute Error:", mae)
+    print("R-squared:", r2)
     return
--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from typing import List, Text, Tuple
 
+from matplotlib import pyplot as plt
+from sklearn.ensemble import GradientBoostingRegressor
 import numpy as np
 import pandas as pd
 from numpy import array
+import joblib
 
 
-def load_data(data_path : Path) -> Tuple:
+def load_data(data_path: Path) -> Tuple:
     init_df = pd.read_excel(data_path, sheet_name='Dataset')
     init_stats_df = pd.read_excel(data_path, sheet_name='Statistics')
     init_corr_df = pd.read_excel(data_path, sheet_name='Correlation')
@@ -30,15 +33,26 @@ def in_out_array(df: pd.DataFrame) -> Tuple[array, array]:
     return np.array(df[df.columns[1:]]), np.array(df[df.columns[0]])
 
 
-def save_model():
+def save_model(model: GradientBoostingRegressor, model_name: str):
+    joblib.dump(model, model_name.join('.pkl'))
     return
 
 
-def load_model():
-    return
+def load_model(model_name: str) -> GradientBoostingRegressor:
+    model = joblib.load(model_name.join('.pkl'))
+    return model
 
 
-def plot():
+def plot(y_pred, y_test, figsize: (int, int), xlabel: str, ylabel: str, title: str):
+    plt.figure(figsize=figsize)
+    x = np.arange(len(y_test))  # Create an array of indices
+    plt.plot(x, y_test, label='Actual', color='blue', marker='o', linestyle='-', markersize=5)
+    plt.plot(x, y_pred, label='Predicted', color='red', marker='x', linestyle='--', markersize=5)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend()
+    plt.show()
     return
 
 
